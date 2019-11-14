b63e3f6a195ea25581bbb2557f52040b44768cb7
Lala Shanks <55359358+lalashanks@users.noreply.github.com>
2019-11-14 14:02:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+const button = document.querySelector('button');
+const input = document.querySelector('input[type="text"]');
+const list = document.querySelector('ul');
+
+button.addEventListener('click', () => {
+  if (input.value != '') {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    list.appendChild(li);
+    input.value = '';
+  }
+});
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === "LI"){
+    e.target.classList.toggle("checked")
+ }
+});
+function removeAll(){
+  while(list.firstChild){
+    list.removeChild(list.firstChild);
+  }
+}
+document.getElementById('deleteAll').addEventListener('click', removeAll);
+
 let turn = 1;
 let clicks = 0;
 let boxes = document.querySelectorAll(".insideBox");
@@ -7,34 +31,3 @@ document.querySelector(".overAllBox").addEventListener("click",item =>{
   turn = (turn==1)? 2 : 1;
   checkIfWinner(x.innerHTML);
 })
-
-function checkIfWinner(whoWon){
-  if (boxes[0].innerHTML==whoWon&&boxes[1].innerHTML==whoWon&&boxes[2].innerHTML==whoWon){
-    alert(whoWon+" is the winner");
-    location.reload();
-  }else if (boxes[3].innerHTML==whoWon&&boxes[4].innerHTML==whoWon&&boxes[5].innerHTML==whoWon) {
-    alert(whoWon+" is the winner");
-    location.reload();
-  }else if (boxes[6].innerHTML==whoWon&&boxes[7].innerHTML==whoWon&&boxes[8].innerHTML==whoWon) {
-    alert(whoWon+" is the winner");
-    location.reload();
-  }else if (boxes[0].innerHTML==whoWon&&boxes[3].innerHTML==whoWon&&boxes[6].innerHTML==whoWon) {
-    alert(whoWon+" is the winner");
-    location.reload();
-  }else if (boxes[1].innerHTML==whoWon&&boxes[4].innerHTML==whoWon&&boxes[7].innerHTML==whoWon) {
-    alert(whoWon+" is the winner");
-    location.reload();
-  }else if (boxes[2].innerHTML==whoWon&&boxes[5].innerHTML==whoWon&&boxes[8].innerHTML==whoWon) {
-    alert(whoWon+" is the winner");
-    location.reload();
-  }else if (boxes[0].innerHTML==whoWon&&boxes[4].innerHTML==whoWon&&boxes[8].innerHTML==whoWon) {
-    alert(whoWon+" is the winner");
-    location.reload();
-  }else if (boxes[2].innerHTML==whoWon&&boxes[4].innerHTML==whoWon&&boxes[6].innerHTML==whoWon) {
-    alert(whoWon+" is the winner");
-    location.reload();
-  }
-  else if(clicks == 9){
-    location.reload(true)
-  }
-}
